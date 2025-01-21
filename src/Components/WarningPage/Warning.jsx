8a49483cf0ad12c2450cc6478ec003d7219beac2
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-01-21 12:08:55 +0500

    New Commit

--- src/Components/WarningPage/Warning.jsx
+++ src/Components/WarningPage/Warning.jsx
@@ -1,10 +1,13 @@
+import { useLocation } from "react-router-dom"
 import Cube from "../../assets/Images/Cube.png"
+import { navbarList } from "../../hooks/useRoute"
 const WarningPage = () => {
+  const path = useLocation()
   return (
     <>
      <div className="bg-slate-800 w-ful flex justify-center items-center h-full overflow-y-auto text-white">
        <div className="flex flex-col justify-center items-start">
-       <h2 className="text-[30px] font-bold"><span className="text-red-600">!</span>This page is under development</h2>
+       <h2 className="text-[30px] font-bold"><span className="text-red-600">!</span>{navbarList.map(item => item.path === path.pathname && item.title)} page is under development...</h2>
           <img src={Cube} alt="Cube" className="mx-auto"/>
        </div>
      </div>
