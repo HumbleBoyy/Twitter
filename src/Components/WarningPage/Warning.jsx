ea595d74552e9c2fa54d068393ecc677104b8272
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-01-21 12:10:21 +0500

    New Commit

--- src/Components/WarningPage/Warning.jsx
+++ src/Components/WarningPage/Warning.jsx
@@ -5,7 +5,7 @@ const WarningPage = () => {
   const path = useLocation()
   return (
     <>
-     <div className="bg-slate-800 w-ful flex justify-center items-center h-full overflow-y-auto text-white">
+     <div className="bg-slate-900 w-ful flex justify-center items-center h-full overflow-y-auto text-white">
        <div className="flex flex-col justify-center items-start">
        <h2 className="text-[30px] font-bold"><span className="text-red-600">!</span>{navbarList.map(item => item.path === path.pathname && item.title)} page is under development...</h2>
           <img src={Cube} alt="Cube" className="mx-auto"/>
