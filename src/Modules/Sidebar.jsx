c9df543719a94d0381649f9685c7f2e82cecb822
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-01-23 17:40:06 +0500

    New Commit

--- src/Modules/Sidebar.jsx
+++ src/Modules/Sidebar.jsx
@@ -10,7 +10,7 @@ const Sidebar = () => {
   const path = useLocation()
   const {postList} = getUserInfo()
   return (
-    <div  className='w-[22%] px-2 py-3  h-auto overflow-y-auto flex flex-col'>
+    <div  className='w-[22%] px-2 py-3  h-[100vh]  overflow-y-auto flex flex-col'>
       <Input  type={'text'} placeholder={"Search twitter"} extraClass={"newClass !w-full !bg-slate-200 !px-7 !rounded-[50px] !text-slate-500 !h-[40px] "}/>
       {path.pathname !== PATH.profile ?
       <>
@@ -19,7 +19,7 @@ const Sidebar = () => {
       </>
       :
         <>
-        <div className='flex flex-wrap  w-full gap-[1px] h-[400px] mt-2 overflow-hidden rounded-[20px]'>
+        <div className='flex flex-wrap  w-full gap-[1px] h-auto mt-2  rounded-[20px]'>
           {postList.map(item => (
             <>
                {item.postPicture !== null ? <img src={item.postPicture} className='object-cover hover:scale-50 cursor-pointer duration-500 w-[100px]' key={item.id} width={123} height={88}/> :""}
