8b91e7c8ea683e23318b7a37213f7ec0a685ae9b
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-01-23 17:14:12 +0500

    New Commit

--- src/Modules/Header.jsx
+++ src/Modules/Header.jsx
@@ -3,6 +3,7 @@ import { useLocation, useNavigate } from 'react-router-dom'
 import { BsStars } from 'react-icons/bs'
 import getUserInfo from '../hooks/getUserInfo'
 import { BiArrowBack } from 'react-icons/bi'
+import { PATH } from '../hooks/usePath'
 
 
 const Header = () => {
@@ -11,20 +12,19 @@ const Header = () => {
     const navigate =  useNavigate()
   return (
    <>
-     {path.pathname !== "/profile" ?
-      <header className='flex justify-between items-center p-5 border-b-2 border-slate-400'>
-      <h2 className='font-bold text-[25px]'>{navbarList.map(item => item.path === path.pathname && item.title)}</h2>
-      <BsStars size={"25px"}/>
-    </header>
-     :
-     <header className='flex items-center gap-5 px-5 py-2 border-b-2 border-slate-400'>
-      <BiArrowBack onClick={()=> navigate(-1)} className='cursor-pointer' size={"25px"}/>
-      <div className='flex flex-col'>
-        <h2 className='font-bold text-[25px]'>{username}</h2>
-        <p className='text-[16px] text-slate-500'>{postList.length} Tweets</p>
-      </div>
-    </header>
-     
+     {path.pathname !== PATH.profile ?
+       <header className='flex justify-between items-center p-5 border-b-2 border-slate-400'>
+        <h2 className='font-bold text-[25px]'>{navbarList.map(item => item.path === path.pathname && item.title)}</h2>
+        <BsStars size={"25px"}/>
+      </header>
+     : 
+        <header className='flex items-center gap-5 px-5 py-2 border-b-2 border-slate-400'>
+          <BiArrowBack onClick={()=> navigate(-1)} className='cursor-pointer' size={"25px"}/>
+          <div className='flex flex-col'>
+            <h2 className='font-bold text-[25px]'>{username}</h2>
+            <p className='text-[16px] text-slate-500'>{postList.length} Tweets</p>
+          </div>
+        </header>
      }
    </>
   )
--- src/Modules/Sidebar.jsx
+++ src/Modules/Sidebar.jsx
@@ -3,13 +3,14 @@ import Input from '../Components/Input/Input'
 import Trends from '../Components/Trends/Trends'
 import Suggestion from '../Components/Suggestions/Suggestion'
 import { useLocation } from 'react-router-dom'
+import { PATH } from '../hooks/usePath'
 
 const Sidebar = () => {
   const path = useLocation()
   return (
     <div  className='w-[22%] px-2 py-3  h-[100vh] overflow-y-auto flex flex-col'>
       <Input  type={'text'} placeholder={"Search twitter"} extraClass={"newClass !w-full !bg-slate-200 !px-7 !rounded-[50px] !text-slate-500 !h-[40px] "}/>
-      {path.pathname !== "/profile" ?
+      {path.pathname !== PATH.profile ?
       <>
        <Trends/>
        <Suggestion/>
