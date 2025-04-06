707fb4313a06fe1132260f2d7c90030321e82a45
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-04-06 22:46:13 +0500

    new Commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,18 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { TokenContext } from './Context/context.jsx'
-
+import Logo from "../src/assets/Logos/twitter.svg"
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <TokenContext>
         <div className='app_wrapper'>
           <App />
+        </div>
+        <div className='message_wrapper'>
+            <div className='fixed inset-0   justify-center top-56 items-center w-full z-0'>
+                 <img className="logo_animated mx-auto" src={Logo} alt="Logo" width={300} height={300}/>
+              </div>
+           <h1 className='text-[20px] font-bold text-white text-center z-50'>Ekraningiz sigimi 1110px-dan kichik!</h1>
         </div>
     </TokenContext>
   </BrowserRouter>
